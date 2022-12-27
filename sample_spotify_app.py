9148162a38558a8ee84e1e30d86f5006878825e3
BlueSideStrongSide <bmanradge@gmail.com>
2022-12-26 19:45:47 -0500

    removing json lib from sample sample_spotify_app.py

--- sample_spotify_app.py
+++ sample_spotify_app.py
@@ -1,7 +1,6 @@
 from source.auth.spotify_oauth_authorization import OauthSpotify_Authorization_Code_Flow
 from source.interact.spotify_api import SpotifyHandler
 import pprint
-import json
 
 spotify_authenticator = OauthSpotify_Authorization_Code_Flow(scopes=["user-read-recently-played", 'user-top-read'])
 
