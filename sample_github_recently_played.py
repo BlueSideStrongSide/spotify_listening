a167d45caa8925cf20a108959defd776d31d6569
BlueSideStrongSide <bmanradge@gmail.com>
2023-05-23 21:41:51 -0400

    updating github sample script

--- sample_github_recently_played.py
+++ sample_github_recently_played.py
@@ -5,4 +5,7 @@ spotify_authenticator = OauthSpotify_Authorization_Code_Flow(scopes=["user-read-
 
 if spotify_authenticator.authenticated:
     spotify_interact = SpotifyHandler(auth_manager=spotify_authenticator)
-    print(spotify_interact.spotify_get_recently_played_tracks())
+    result = spotify_interact.spotify_get_recently_played_tracks()
+
+    for track in result.tracks:
+        print(f'Track Name: {track.track_name} Artist: {track.artist_name} Explicit: {str(track.explicit)}')
