8ee0c463c0c1edf4dd3dc123d4fef0b001afe0ad
BlueSide_StrongSide <bmanradge@gmail.com>
2023-10-31 23:29:23 -0400

    Update sample_github_recently_played.py
    removed certain tracks from the recently played tracker

--- sample_github_recently_played.py
+++ sample_github_recently_played.py
@@ -10,10 +10,9 @@ if spotify_authenticator.authenticated:
     result = spotify_interact.spotify_get_recently_played_tracks()
 
     for track in result.tracks:
-        with open("recently_played_tracks.txt", "a+") as out_file:
-            out_file.write(f'GitHub_Runner_Date: {datetime.datetime.utcnow()} '
-                f'Track Name: "{track.track_name}" '
-                f'Artist Name: "{track.artist_name}" '
-                f'Web Link: {track.ext_spotify_url} '
-                f'Explicit: {track.explicit} \n'
-                  )
+        if not track.explicit:
+            with open("recently_played_tracks.txt", "a+") as out_file:
+                out_file.write(f'GitHub_Runner_Date: {datetime.datetime.utcnow()} '
+                    f'Track Name: "{track.track_name}" '
+                    f'Artist Name: "{track.artist_name}" '
+                    f'Web Link: {track.ext_spotify_url} ')
